755727113ab9241ce45af495a209e2c24a47da9f
mphilpot <mphilpot@d39efdd8-4ae1-1840-9d84-35bc49fd584b>
2005-08-15 02:15:37 +0000

    git-svn-id: http://sid.zencoding.net/svn/Websites/zencoding/trunk@1 d39efdd8-4ae1-1840-9d84-35bc49fd584b

---
+++ projects/tracker/meta_inc.php
@@ -0,0 +1,17 @@
+<?php
+	# Mantis - a php based bugtracking system
+	# Copyright (C) 2000 - 2002  Kenzaburo Ito - kenito@300baud.org
+	# Copyright (C) 2002 - 2004  Mantis Team   - mantisbt-dev@lists.sourceforge.net
+	# This program is distributed under the terms and conditions of the GPL
+	# See the README and LICENSE files for details
+
+	# --------------------------------------------------------
+	# $Id: meta_inc.php,v 1.17 2005/03/31 01:08:37 thraxisp Exp $
+	# --------------------------------------------------------
+
+?>
+	<meta http-equiv="Pragma" content="no-cache" />
+	<meta http-equiv="Cache-Control" content="no-cache" />
+	<meta http-equiv="Pragma-directive" content="no-cache" />
+	<meta http-equiv="Cache-Directive" content="no-cache" />
+	<meta http-equiv="Expires" content="<?php echo gmdate( 'D, d M Y H:i:s \G\M\T', time() ) ?>" />
