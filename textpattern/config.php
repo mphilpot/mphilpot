9a8295c30ce2f7d05fb393a27e04f49fd2ddf789
mphilpot <mphilpot@d39efdd8-4ae1-1840-9d84-35bc49fd584b>
2007-10-28 23:53:50 +0000

    staticmatic version check!
    git-svn-id: http://sid.zencoding.net/svn/Websites/zencoding/trunk@32 d39efdd8-4ae1-1840-9d84-35bc49fd584b

--- textpattern/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$txpcfg['db'] = 'mphilpot_blog';
-$txpcfg['user'] = 'mphilpot_admin';
-$txpcfg['pass'] = '6t79n0XX';
-$txpcfg['host'] = 'localhost';
-$txpcfg['table_prefix'] = 'txt_';
-$txpcfg['txpath'] = '/home/mphilpot/public_html/textpattern';
-$txpcfg['dbcharset'] = 'latin1';
-?>
